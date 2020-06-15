70c394336c9f332f1479cf11031d0d77f2dcb1d2
Julien <jul77.me@hotmail.fr>
2020-06-15 21:24:37 +0200

    Ajout du process de connexion et d'inscription

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_06_02_192654) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "username", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -39,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_06_02_192654) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "comments", "posts"
